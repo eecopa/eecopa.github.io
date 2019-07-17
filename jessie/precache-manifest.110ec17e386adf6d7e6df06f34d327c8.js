634c378259e00634451dc8fc7b915571bcaa2429
Elias Emanuel Copa <eliasemanuelcopa@outlook.com>
2019-07-17 16:04:56 -0300

    feat: Added jessie web

---
+++ jessie/precache-manifest.110ec17e386adf6d7e6df06f34d327c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e879984fc0105739880bea89ed48cf3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "655f05ccaa5c7caf1a8b",
+    "url": "/static/css/main.381ae652.chunk.css"
+  },
+  {
+    "revision": "8e438eb8cfd42f7a8993",
+    "url": "/static/js/2.8081392b.chunk.js"
+  },
+  {
+    "revision": "655f05ccaa5c7caf1a8b",
+    "url": "/static/js/main.fba212d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "697d0fb168565de8934ddde93acefeda",
+    "url": "/static/media/ambito.697d0fb1.png"
+  }
+]);
