4c1cb02915c7f01d1f9a1013c3dfe0b3c074c1ff
Elias Emanuel Copa <eliasemanuelcopa@outlook.com>
2019-07-17 17:07:39 -0300

    fix: jessie page

---
+++ jessie/precache-manifest.3e170f8da944e1ff15ac3748510c5eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf13c7db33e6e54bdf3ede9d50b2737a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cbef757af4adc07b2ac",
+    "url": "/static/css/main.8b7445ab.chunk.css"
+  },
+  {
+    "revision": "8e438eb8cfd42f7a8993",
+    "url": "/static/js/2.8081392b.chunk.js"
+  },
+  {
+    "revision": "4cbef757af4adc07b2ac",
+    "url": "/static/js/main.fba212d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "697d0fb168565de8934ddde93acefeda",
+    "url": "/static/media/ambito.697d0fb1.png"
+  }
+]);
